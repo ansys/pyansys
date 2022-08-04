54a8de5879cc6188e6c8297aef0a4ff719f6317e
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2022-08-04 11:57:17 +0200

    Create metapackage (#35)
    * Mv PyAnsys folder to src/pyansys and remove build folder
    
    * Adapt gitignore
    
    * Point docs to proper folder
    
    * Adapt to pyproject.toml and create install configs
    
    * Import version in __init__ file
    
    * "all" dependencies should be explicitly mentioned
    
    * WIP on poetry
    
    * Implementing pre-commit + cleaning reqs
    
    * Add vale config
    
    * Removing reqs file
    
    * Updating vale accepted words
    
    * Creating an actual CI
    
    * Update docs build stage
    
    * Change jobs description
    
    * Update .github/workflows/ci-build.yml
    
    Co-authored-by: Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
    
    * Removing _version and simplifying things
    
    * Remove unnecessary line
    
    * Simplifyinf conf.py
    
    * Generate wheelhouse for different targets
    
    * Missing target in extras-import stage for wheel build
    
    * Limit to Python 3 for now
    
    * Wheelhouse already generates all imports
    
    * Comment problematic dependencies for now
    
    * Install wheelhouse when needed
    
    * Remove Python 3.10 version for now
    
    * Improve description of extras jobs
    
    * Upgrade pip
    
    * Remove pip upgrades - did not solve the problem
    
    * Update pyproject.toml
    
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
    
    * Apply suggestions from code review by @akaszynski
    
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>
    
    * Commiting @jorgepiloto's suggestion
    
    * Pre-commit check
    
    * Apply suggestions from code review
    
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
    
    Co-authored-by: Jorge Martínez <28702884+jorgepiloto@users.noreply.github.com>
    Co-authored-by: Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
    Co-authored-by: Alex Kaszynski <akascap@gmail.com>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,26 +1,20 @@
 """Configuration file for docs.pyansys.com landing page."""
 from datetime import datetime
-from io import open as io_open
-import os
 
 from ansys_sphinx_theme import pyansys_logo_black
 
-project = 'pyansys-landing'
+from pyansys import __version__ as pyansys_version
+
+project = "pyansys-landing"
 copyright = f"(c) {datetime.now().year} ANSYS, Inc. All rights reserved"
-author = 'ANSYS Inc.'
+author = "ANSYS Inc."
 
-# Get version from version info: execute file from raw string
-__version__ = None
-this_file = os.path.dirname(__file__)
-version_file = os.path.join(this_file, '..', '..', 'pyansys', '_version.py')
-with io_open(version_file, mode='r') as fd:
-    exec(fd.read())
-
-release = version = __version__
+# get the PyAnsys version
+release = version = pyansys_version
 
 # use the default pyansys logo
 html_logo = pyansys_logo_black
-html_theme = 'ansys_sphinx_theme'
+html_theme = "ansys_sphinx_theme"
 
 # specify the location of your github repo
 html_theme_options = {
@@ -35,7 +29,7 @@ html_short_title = html_title = "PyAnsys"
 extensions = []
 
 # The suffix(es) of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
---
+++ src/pyansys/__init__.py
@@ -0,0 +1,10 @@
+"""PyAnsys general package __init__ file."""
+
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:  # pragma: no cover
+    import importlib_metadata
+
+# Read from the pyproject.toml
+# major, minor, patch
+__version__ = importlib_metadata.version("pyansys")
