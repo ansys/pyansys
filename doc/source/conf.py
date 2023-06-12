734cce6c49c61f55b551f0d3680328e8f93f833e
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-06-12 12:59:35 +0200

    fix: pre-commit

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 import os
 
-from ansys_sphinx_theme import pyansys_logo_black, get_version_match
+from ansys_sphinx_theme import get_version_match, pyansys_logo_black
 from sphinx.builders.latex import LaTeXBuilder
 
 from pyansys import __version__ as pyansys_version
