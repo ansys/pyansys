7c3f90c789f1ec89e944671b9ac168e76abf5ef6
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-07-03 11:43:03 +0200

    fix: pre-commit

--- tools/links.py
+++ tools/links.py
@@ -1,3 +1,5 @@
+"""Script for automatic replacement of released links."""
+
 import os
 import re
 
@@ -7,38 +9,44 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PYPROJECT_TOML_FILE = os.path.join(ROOT_DIR, "pyproject.toml")
 """Path to pyproject.toml file."""
 
-DOCS_DIRECTORY =  os.path.join(ROOT_DIR, "doc", "source")
+DOCS_DIRECTORY = os.path.join(ROOT_DIR, "doc", "source")
 """Path to the documentation source directory"""
 
 LINKS = {
-    "ansys-mapdl-core" : "https://mapdl.docs.pyansys.com/version/stable",
-    "ansys-mapdl-reader" : "https://reader.docs.pyansys.com/version/stable",
-    "ansys-dpf-core" : "https://dpf.docs.pyansys.com/version/stable",
-    "ansys-dpf-post" : "https://post.docs.pyansys.com/version/stable",
-    "ansys-dpf-gate" : None,
-    "ansys-dpf-composites" : "https://composites.dpf.docs.pyansys.com/version/stable",
-    "ansys-fluent-core" : "https://fluent.docs.pyansys.com/version/stable",
-    "ansys-fluent-visualization" : "https://visualization.fluent.docs.pyansys.com/version/stable",
-    "ansys-fluent-parametric" : "https://parametric.fluent.docs.pyansys.com/version/stable",
-    "pyaedt" : "https://aedt.docs.pyansys.com/version/stable",
-    "ansys-platform-instancemanagement" : None,
-    "ansys-grantami-bomanalytics" : "https://bomanalytics.grantami.docs.pyansys.com/version/stable",
-    "ansys-grantami-recordlist" : "https://recordlists.grantami.docs.pyansys.com/version/stable",
-    "ansys-openapi-common" : None,
-    "ansys-seascape" : "https://seascape.docs.pyansys.com/version/stable",
-    "ansys-sherlock-core" : "https://sherlock.docs.pyansys.com/version/stable",
-    "ansys-meshing-prime" : "https://prime.docs.pyansys.com/version/stable",
-    "pytwin" : "https://twin.docs.pyansys.com/version/stable",
-    "ansys-systemcoupling-core" : "https://systemcoupling.docs.pyansys.com/version/stable",
-    "ansys-motorcad-core" : "https://motorcad.docs.pyansys.com/version/stable",
-    "ansys-math-core" : "https://aedt.docs.pyansys.com/version/stable",
-    "ansys-optislang-core" : "https://optislang.docs.pyansys.com/version/stable",
-    "ansys-mechanical-core" : "https://mechanical.docs.pyansys.com/version/stable",
+    "ansys-mapdl-core": "https://mapdl.docs.pyansys.com/version/stable",
+    "ansys-mapdl-reader": "https://reader.docs.pyansys.com/version/stable",
+    "ansys-dpf-core": "https://dpf.docs.pyansys.com/version/stable",
+    "ansys-dpf-post": "https://post.docs.pyansys.com/version/stable",
+    "ansys-dpf-gate": None,
+    "ansys-dpf-composites": "https://composites.dpf.docs.pyansys.com/version/stable",  # noqa: E501
+    "ansys-fluent-core": "https://fluent.docs.pyansys.com/version/stable",
+    "ansys-fluent-visualization": "https://visualization.fluent.docs.pyansys.com/version/stable",  # noqa: E501
+    "ansys-fluent-parametric": "https://parametric.fluent.docs.pyansys.com/version/stable",  # noqa: E501
+    "pyaedt": "https://aedt.docs.pyansys.com/version/stable",
+    "ansys-platform-instancemanagement": None,
+    "ansys-grantami-bomanalytics": "https://bomanalytics.grantami.docs.pyansys.com/version/stable",  # noqa: E501
+    "ansys-grantami-recordlist": "https://recordlists.grantami.docs.pyansys.com/version/stable",  # noqa: E501
+    "ansys-openapi-common": None,
+    "ansys-seascape": "https://seascape.docs.pyansys.com/version/stable",
+    "ansys-sherlock-core": "https://sherlock.docs.pyansys.com/version/stable",
+    "ansys-meshing-prime": "https://prime.docs.pyansys.com/version/stable",
+    "pytwin": "https://twin.docs.pyansys.com/version/stable",
+    "ansys-systemcoupling-core": "https://systemcoupling.docs.pyansys.com/version/stable",  # noqa: E501
+    "ansys-motorcad-core": "https://motorcad.docs.pyansys.com/version/stable",
+    "ansys-math-core": "https://aedt.docs.pyansys.com/version/stable",
+    "ansys-optislang-core": "https://optislang.docs.pyansys.com/version/stable",  # noqa: E501
+    "ansys-mechanical-core": "https://mechanical.docs.pyansys.com/version/stable",  # noqa: E501
 }
-"""Dictionary containing key-value pairs of the PyAnsys packages and their multi-version docs site."""
+"""Dictionary with PyAnsys packages and their multi-version docs site."""
+
 
 def retrieve_major_minor(package: str):
-    """Extract the major and minor version of a pinned package in the pyproject.toml file.
+    """Extract the major and minor version of a pinned package.
+
+    Notes
+    -----
+    This function navigates to the packages pyproject.toml file
+    and processes it to retrieve the desired major, minor versions.
 
     Parameters
     ----------
@@ -50,10 +58,9 @@ def retrieve_major_minor(package: str):
     tuple of (int, int)
         The major and minor versions of the package
     """
-    
-    with open(PYPROJECT_TOML_FILE, 'r') as file:
+    with open(PYPROJECT_TOML_FILE, "r") as file:
         content = file.read()
-        pattern = r'\b' + re.escape(package) + r'==(\d+)\.(\d+)'
+        pattern = r"\b" + re.escape(package) + r"==(\d+)\.(\d+)"
         match = re.search(pattern, content)
 
         if match:
@@ -63,8 +70,9 @@ def retrieve_major_minor(package: str):
 
     return None
 
-def search_and_replace(link : str, new_link: str):
-    """Replaces existing link with the newly provided link.
+
+def search_and_replace(link: str, new_link: str):
+    """Replace existing link with the newly provided link.
 
     Parameters
     ----------
@@ -73,32 +81,31 @@ def search_and_replace(link : str, new_link: str):
     new_link : str
         The link to replace the existing one.
     """
-    
     # Traverse the docs directory
     for root, _, files in os.walk(DOCS_DIRECTORY):
-
         # Skip the _static subdirectory
         if "_static" in root.split(os.sep):
-            continue  
+            continue
 
         # Process the files
         for file in files:
             file_path = os.path.join(root, file)
-            with open(file_path, 'r') as f:
+            with open(file_path, "r") as f:
                 content = f.read()
-            
+
             # Search for the link in the content, replace and save
             if link in content:
                 new_content = content.replace(link, new_link)
-                
-                with open(file_path, 'w') as f:
+
+                with open(file_path, "w") as f:
                     f.write(new_content)
-                    
-                print(f"Replaced '{link}' with '{new_link}' in file: {file_path}")
+
+                print(f"Replaced '{link}' with '{new_link}' in file: {file_path}")  # noqa: E501
+
 
 def released_docs():
-    """Update links for released documentation
-    
+    """Update links for released documentation.
+
     Notes
     -----
     Links are expected to point to a certain version when released
@@ -107,24 +114,23 @@ def released_docs():
     be run locally and the changes have to be committed to the release branch
     prior to releasing.
     """
-    
     # Loop over all the above defined packages
     for key, value in LINKS.items():
-        
         # Packages that are not adapted to multi-version have a None
         # link associated as its value... skip them.
         if value is None:
             continue
-        
+
         # Retrieve the major and minor versions of the package
         major, minor = retrieve_major_minor(key)
-        
+
         # Define the new link
         link_root = value.split("/version/")[0]
         new_link = f"{link_root}/version/{major}.{minor}"
-        
+
         # Search and replace through all our docs links
         search_and_replace(value, new_link)
+
 
 if __name__ == "__main__":
     released_docs()
